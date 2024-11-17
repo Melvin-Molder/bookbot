8b19ea2d7dda69ad8a64c8455a803d3f6fee91c6
MMolder-Code <molder.melvin@gmail.com>
2024-11-17 13:27:48 -0500

    book report

--- main.py
+++ main.py
@@ -1,15 +1,53 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        print(file_contents)        
-    print(count_words(file_contents))
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    list_dict = dict_to_list(chars_dict)
+    create_report(list_dict, num_words, book_path)
+    # print(list_dict)
+    
+
+def create_report(list_dict, word_count, path):
+    print(f"--- Begin report of {path} ---")
+    print(f"{word_count} words found in the document")
+    print()
+    for i in range(len(list_dict) - 1):
+        if list_dict[i]["key"].isalpha():
+            print(f"The {list_dict[i]["key"]} character was found {list_dict[i]["value"]} times")
 
 
-def count_words(text):
-    count = 0
+def dict_to_list(dict):
+    list_dict = []
+    for key in dict:
+        list_dict.append({'key': key, 'value': dict[key]})
+    list_dict.sort(reverse=True, key=sort_on)
+    return list_dict
+
+
+def sort_on(dict):
+    return dict["value"]
+
+
+def get_num_words(text):
     words = text.split()
-    for word in words:
-        count += 1
-    return count
+    return len(words)
+
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
 
 main()

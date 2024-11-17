8272a551b98c993ae7ec2737868f54976b918bba
MMolder-Code <molder.melvin@gmail.com>
2024-11-17 12:11:46 -0500

    print word count

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(file_contents)
+        print(file_contents)        
+    print(count_words(file_contents))
+
+
+def count_words(text):
+    count = 0
+    words = text.split()
+    for word in words:
+        count += 1
+    return count
 
 main()
